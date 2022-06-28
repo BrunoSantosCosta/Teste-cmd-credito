9595df57aee151e7a4a84647a452f94ff940240b
BrunoSantosCosta <brunosanttos667@gmail.com>
2022-06-28 19:31:27 -0300

    Install nodemon

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (request, response) => {
-  response.send('Deu bom');
+  response.send('Deu n');
 })
 
 app.listen(3001,() => console.log('Server started at http://localhost:3001'))
