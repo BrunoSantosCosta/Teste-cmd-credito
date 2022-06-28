fc1838e5ea99e24fd031a54beb01b10fb73a0f6f
BrunoSantosCosta <brunosanttos667@gmail.com>
2022-06-28 19:49:34 -0300

    ESlint and editor config

--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ const app = express();
 
 app.get('/', (request, response) => {
   response.send('Deu n');
-})
+});
 
-app.listen(3001,() => console.log('Server started at http://localhost:3001'))
+app.listen(3001, () => console.log('Server started at http://localhost:3001'));
